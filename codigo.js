d110e26b96d1e01eac7eb1546b2480bd3128616b
RaianKim <raiank2007@gmail.com>
2024-07-11 03:42:05 -0300

    .

--- codigo.js
+++ codigo.js
@@ -12,67 +12,63 @@ let Tareas = [
     }*/
 ]
 
-Agregar = () =>
-{
-  let aux = document.getElementById("textoTarea").value;
-  Tareas.push({Nombre: aux,Check: false,TiempoCreado: Date.now(),TiempoTerminado: null});
-  MostrarHtml();
+Agregar = () => {
+    let aux = document.getElementById("textoTarea").value;
+    Tareas.push({ Nombre: aux, Check: false, TiempoCreado: Date.now(), TiempoTerminado: null });
+    MostrarHtml();
 }
 
-deshabilitar = (aux,tarea,num) =>
-{
+deshabilitar = (aux, tarea, num) => {
     let output = document.getElementById(aux);
-    output.innerHTML="";
-    output.innerHTML+=`<div id="cb${aux}"><li id="cb${aux}"style="text-decoration:line-through;color: gray;">${tarea}<input class="form-check-input" type="checkbox" id="flexCheckChecked " onclick="deshabilitar('${aux}',${tarea})" Checked disabled> </li></div>`
-    Tareas[num].Check = true; 
-    Tareas[num].TiempoTerminado = ((Date.now() - Tareas[num].TiempoCreado)/1000).toFixed(1);
+    output.innerHTML = "";
+    output.innerHTML += `<div id="cb${aux}" ><li class="divTarea" id="cb${aux}"style="text-decoration:line-through;color: gray;"><input type="checkbox" id="flexCheckChecked " onclick="deshabilitar('${aux}',${tarea})" Checked disabled><p>${tarea}</p></li></div>`
+    Tareas[num].Check = true;
+    Tareas[num].TiempoTerminado = ((Date.now() - Tareas[num].TiempoCreado) / 1000).toFixed(1);
 }
 
-tareaMasRapida = () =>
-{
+tareaMasRapida = () => {
     let auxNum = 0;
     let auxTar = null;
     Tareas.forEach(t => {
 
-        if(t.Check)
-        {
-            if(auxNum == 0)
-            {
+        if (t.Check) {
+            if (auxNum == 0) {
                 auxNum = t.TiempoTerminado;
                 auxTar = t.Nombre
             }
-            if(t.TiempoTerminado < auxNum)
-            {
+            if (t.TiempoTerminado < auxNum) {
                 auxNum = t.TiempoTerminado
                 auxTar = t.Nombre
-                
+
             }
         }
-        console.log(t.TiempoTerminado,t.Nombre)
+        console.log(t.TiempoTerminado, t.Nombre)
     });
-    document.getElementById("tareaMasRapida").innerHTML =`La tarea mas rapida en resolverse fue: ${auxTar}, con ${auxNum} segundos`
+    document.getElementById("tareaMasRapida").innerHTML = `La tarea mas rapida en resolverse fue: ${auxTar}, con ${auxNum} segundos`
 }
 
-MostrarHtml = () =>
-{
+BorrarLista = () => {
+    let output = document.getElementById("tareas-ul")
+    output.innerHTML = "";
+    Tareas=[];
+}
+MostrarHtml = () => {
     let output = document.getElementById("tareas-ul")
     output.innerHTML = "";
     let tachadoONo = "";
     let checkeadoONo = "";
-    let aux=0;
+    let aux = 0;
     Tareas.forEach(n => {
-        if(n.Check)
-        {
+        if (n.Check) {
             tachadoONo = "text-decoration:line-through;color: gray;"
             checkeadoONo = "checked disabled"
         }
-        else
-        {
-            tachadoONo=""
-            checkeadoONo=""
+        else {
+            tachadoONo = ""
+            checkeadoONo = ""
         }
-        
-        output.innerHTML+=`<div id="cb${aux}"><li style="${tachadoONo}">${n.Nombre}<input class="form-check-input" type="checkbox" id="flexCheckChecked " onclick="deshabilitar('cb${aux}','${n.Nombre}',${aux})" ${checkeadoONo}> </li></div>`
+
+        output.innerHTML += `<div id="cb${aux}" ><li style="${tachadoONo}"class="divTarea"><input type="checkbox" id="flexCheckChecked " onclick="deshabilitar('cb${aux}','${n.Nombre}',${aux})" ${checkeadoONo}><p>${n.Nombre}</p></li></div>`
         aux++;
     });
     document.getElementById('textoTarea').value = '';
